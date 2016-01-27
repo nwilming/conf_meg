1bcf2abd27d0dce4517dce9045a687739ed2080f
Niklas Wilming <niklaswilming@fastmail.fm>
2016-01-27 18:43:42 +0100

    Modify mfiles to preprocess only blocks of trials

--- mfiles/get_filenames.m
+++ mfiles/get_filenames.m
@@ -1,4 +1,4 @@
-function dataset = get_filenames(subject, session, varargin)
+function dataset = get_filenames(subject, session, block, varargin)
 
 path = default_arguments(varargin, 'path', '/Volumes/dump/conf_meg/');
 
@@ -7,4 +7,4 @@ mkdir(path);
 
 type = default_arguments(varargin, 'type', 'confidence');
 
-dataset = fullfile(path, sprintf('%02i_%02i_%s.mat', subject, session, type));
+dataset = fullfile(path, sprintf('%02i_%02i_%02i_%s.mat', subject, session, block, type));
--- mfiles/preproc.m
+++ mfiles/preproc.m
@@ -1,28 +1,42 @@
 function preproc(subject, session)
 dataset = get_raw_filenames(subject, session);
-[data, cfg] = prepare_dataset(dataset, subject, session, 'trialdef');
-artifacts = reject_artifacts(dataset, data);
-cfg.artfctdef = artifacts;
 
-% Compute the time period of interest: Between ref onset and decision. 
-event_fields = {'start', 'correct', 'correct_v', 'noise_sigma', 'noise_sigma_v',...
-    'ref_onset', 'ref_offset', 'stim_onset', 'cc1', 'cc2', 'cc3',...
-    'cc4', 'cc5', 'cc6', 'cc7', 'cc8', 'cc9', 'cc10', 'stim_offset', 'response',...
-    'response_v', 'feedback', 'feedback_v', 'end'};
-
-trial_onset = cfg.trl(:, 1);
-ref_onset = cfg.trl(:, find(strcmp('ref_onset', event_fields)) + 3);
-response = cfg.trl(:, find(strcmp('response', event_fields)) + 3);
-
-cfg.artfctdef.crittoilim = [ref_onset, response]/1200;
-cfg.artfctdef.reject = 'complete';
-
-datat = ft_redefinetrial(cfg, data);
-clean = ft_rejectartifact(cfg, datat);
-
-samplerows = find(clean.trialinfo(1,:)>255);
-clean = downsample(clean, samplerows);
-savepath = get_filenames(subject, session, 'confidence');
-clear data datat
-save(savepath)
+% First of all get trial struct
+cfg = get_trials(dataset, 'trialdef');
+blocks = get_blocks(cfg.trl(:, end));
+block_nums = unique(blocks);
+for block = 1:length(block_nums)
+    id_block = cfg.trl(:,end) == block;
+    trls = cfg.trl(id_block, 1:2);
+    block_start = min(trls(:)) - 2500;
+    block_end = max(trls(:)) + 2500;
+    
+    [data, cfg_block] = prepare_dataset(dataset, subject, session, block_start, block_end);
+    cfg_block.trl = cfg.trl(id_block, :);
+    artifacts = reject_artifacts(dataset, data);
+    cfg_block.artfctdef = artifacts;
+    
+    % Compute the time period of interest: Between ref onset and decision.
+    event_fields = {'start', 'correct', 'correct_v', 'noise_sigma', 'noise_sigma_v',...
+        'ref_onset', 'ref_offset', 'stim_onset', 'cc1', 'cc2', 'cc3',...
+        'cc4', 'cc5', 'cc6', 'cc7', 'cc8', 'cc9', 'cc10', 'stim_offset', 'response',...
+        'response_v', 'feedback', 'feedback_v', 'end'};
+    
+    trial_onset = cfg_block.trl(:, 1);
+    ref_onset = cfg_block.trl(:, find(strcmp('ref_onset', event_fields)) + 3);
+    response = cfg_block.trl(:, find(strcmp('response', event_fields)) + 3);
+    
+    cfg_block.artfctdef.crittoilim = [ref_onset, response]/1200;
+    cfg_block.artfctdef.reject = 'complete';
+    
+    datat = ft_redefinetrial(cfg_block, data);
+    clear data
+    clean = ft_rejectartifact(cfg_block, datat);
+    
+    samplerows = find(clean.trialinfo(1,:)>255);
+    clean = downsample(clean, samplerows);
+    savepath = get_filenames(subject, session, 'confidence');
+    clear datat
+    save(savepath, '-v7.3')
+end
 end
