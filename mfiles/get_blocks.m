1a6dd66d20be3fe6994cc8a1038bbb4cb4bdec8a
Niklas Wilming <niklaswilming@fastmail.fm>
2016-01-28 17:31:36 +0100

    Sync

--- mfiles/get_blocks.m
+++ mfiles/get_blocks.m
@@ -5,7 +5,6 @@ blocks(1) = 1;
 cur_block = 1;
 for k = 2:length(trials)
     if trials(k-1)+1 ~= trials(k)
-        cur_block
         cur_block = cur_block + 1;
     end
     blocks(k) = cur_block;
