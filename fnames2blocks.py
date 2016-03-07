a509d64f2cea29dbc933d0804de8855550e10a4b
Niklas Wilming <niklaswilming@fastmail.fm>
2016-03-07 11:31:02 +0100

    Make ordering explicit and add a few checks for consistency

--- fnames2blocks.py
+++ fnames2blocks.py
@@ -8,23 +8,39 @@ locale.setlocale(locale.LC_ALL, 'en_US')
 print 'sb2fname = {'
 for subject in ['S%02i'%i for i in range(1, 15)]:
     edf, mf, s = le.listfiles('/Users/nwilming/u/conf_data/%s'%subject)
-    edf = dict((k, edf[k]) for k in sorted(edf))
-    mf = dict((k, mf[k]) for k in sorted(mf))
-    d = {}
 
+    edf = dict((edf.keys()[k], edf[edf.keys()[k]]) for k in argsort([time.mktime(k) for k in edf.keys()]))
+    mf = dict((mf.keys()[k], mf[mf.keys()[k]]) for k in argsort([time.mktime(k) for k in mf.keys()]))
+    d = {}
+    mfkeys = mf.keys()
+    edfkeys = edf.keys()
     # For each day list files sorted by time
-    unique_days = unique([int('%s%s%s'%(t.tm_year, t.tm_mon, t.tm_mday)) for t in edf.keys()])
+    unique_days = unique([int('%s%s%s'%(t.tm_year, t.tm_mon, t.tm_mday)) for t in edfkeys])
 
     days2sessions = dict((d, i+1) for i, d in enumerate(sorted(unique_days)))
 
-    for ei, mi in zip(argsort([time.mktime(a) for a in edf.keys()]), argsort([time.mktime(b) for b in mf.keys()])):
-        e = edf.keys()[ei]
+    ordered_edf = [edfkeys[k] for k in argsort([time.mktime(a) for a in edfkeys])]
+    ordered_mf = [mfkeys[k] for k in argsort([time.mktime(b) for b in mfkeys])]
+
+    assert len(ordered_edf) == len(ordered_mf)
+
+    # Check that time order between consecutive edfs and mfs and edf and mf is correct
+    for i in range(len(ordered_edf)-1):
+        if not time.mktime(ordered_edf[i]) < time.mktime(ordered_edf[i+1]):
+            raise RuntimeErrro('EDF order not correct: %s'%edf[ordered_edf[i]])
+        if not time.mktime(ordered_mf[i]) < time.mktime(ordered_mf[i+1]):
+            raise RuntimeError('EDF order not correct: %s'%edf[ordered_mf[i]])
+        if not time.mktime(ordered_edf[i]) < time.mktime(ordered_mf[i]):
+            raise RuntimeError('EDF after Matlab:', time.mktime(ordered_edf[i]), time.mktime(ordered_mf[i]))
+
+
+    for e, m in zip(ordered_edf, ordered_mf):
         day = int('%s%s%s'%(e.tm_year, e.tm_mon, e.tm_mday))
         session = days2sessions[day]
         if not session in d.keys():
-            d[session] = [(edf.values()[ei], mf.values()[mi])]
+            d[session] = [(edf[e], mf[m])]
         else:
-            d[session].append((edf.values()[ei], mf.values()[mi]))
+            d[session].append((edf[e], mf[m]))
 
     print "\t'%s':{"%(subject)
     for session, files in d.iteritems():
@@ -39,5 +55,5 @@ print '\n'
 print '''
 fnames2sb = {}
 for sub, d in sb2fname.iteritems():
-    fnames2sb = dict((v[0].split('/')[-1], (session, block, v[1])) for session, bvs in d.iteritems() for block, v in bvs.iteritems())
+    fnames2sb[sub] = dict((v[0].split('/')[-1], (session, block, v[1])) for session, bvs in d.iteritems() for block, v in bvs.iteritems())
 '''
