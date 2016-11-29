75205dc36c1fb1eca3fdf29435aa72dee7c7cda1
Niklas Wilming <niklaswilming@fastmail.fm>
2016-11-29 18:40:37 +0100

    Improve tfr_script and to_cluster

--- to_cluster.py
+++ to_cluster.py
@@ -7,7 +7,7 @@ import os
 
 # 2. Create workers
 parser = argparse.ArgumentParser()
-parser.add_argument("script", help='Which python script?')
+parser.add_argument("script", help='Which command to run?')
 parser.add_argument("--cores", help="How many cores do you need?",
                     type=int, default=1)
 parser.add_argument("--memory", help="How many GB ram?", default=12, type=int)
@@ -17,19 +17,19 @@ args = parser.parse_args()
 
 
 command = '''
-#!/bin/sh
+#
 # walltime: defines maximum lifetime of a job
 # nodes/ppn: how many nodes (usually 1)? how many cores?
 
 #PBS -q batch
 #PBS -l walltime=%i:00:00
-#PBS -l nodes=1:ppn=%s
+# -l nodes=1:ppn=%s
 #PBS -l mem=%igb
 # -- run in the current working (submission) directory --
 cd %s
 
 # FILE TO EXECUTE
-python %s 1> cluster/$PBS_JOBID.out 2> cluster/$PBS_JOBID.err
+%s 1> cluster/$PBS_JOBID.out 2> cluster/$PBS_JOBID.err
 '''%(args.walltime, args.cores, args.memory, os.getcwd(), args.script)
 
 print command
