679fd1a93d4d5c7e6c2a5eb7c60a36bfe48c63da
Niklas Wilming <niklaswilming@fastmail.fm>
2017-02-01 16:02:59 +0100

    Make decoding on TFRs possible

--- do_decoding.py
+++ do_decoding.py
@@ -2,47 +2,58 @@ import sys
 sys.path.append('/home/nwilming/')
 from conf_analysis.meg import preprocessing, decoding
 from conf_analysis.behavior import empirical, metadata, keymap
-from distributed import Executor, as_completed
-from distributed import diagnostics
+
 import cPickle
 import mne, locale
 import pandas as pd
 import numpy as np
-locale.setlocale(locale.LC_ALL, "en_US")
-
-executor = Executor("172.18.101.120:8786")
 
 
 block_map = cPickle.load(open('meg/blockmap.pickle'))
 
-results = []
 
-q = []
+def modification_date(filename):
+    try:
+        t = os.path.getmtime(filename)
+        return datetime.datetime.fromtimestamp(t)
+    except OSError:
+        return datetime.datetime.strptime('19700101', '%Y%m%d')
 
-def expand(x):
+
+def contrast_transform(row, j):
+    row = np.vstack(np.asarray(row))
+    if row.shape==(10,1):
+        return row[j].mean()>0.5
+    else:
+        vals = np.vstack(row)
+        return vals[:, j].mean(1)>0.5
+def execute(x):
     print 'Starting task:', x
     sensors = decoding.sensors[x[3]]
     print sensors
-    func = lambda x,y: decoding.generalization_matrix(x, y, 12, slices=np.mean)
-    res = decoding.apply_decoder(func, x[0], x[1], x[2], channels=sensors)
-    res.loc[:, 'sensors'] = x[3]
-    res.loc[:, 'snum'] = x[0]
-    res.loc[:, 'epoch'] = x[1]
-    res.loc[:, 'label'] = x[2]
-    filename = 'results/decoding' + str(x[0]) + x[1] + x[2] + x[3] + '.hdf'
-    res.to_hdf(filename, 'decoding')
-    res = res.groupby([u'predict_time', u'train_time', 'sensors', u'snum', u'epoch', 'label']).mean().reset_index()
-    return res
+    results = []
+    for j, s in enumerate([slice(i, i+1) for i in range(10)] + [slice(0, 10)]):
+        func = lambda x,y: (decoding.generalization_matrix(x, y, 12,
+            slices='reshape'))
+        res = decoding.apply_decoder(func, x[0], x[1], x[2],
+            channels=sensors, label_func=lambda x: contrast_transform(x, j))
+        res.loc[:, 'sensors'] = x[3]
+        res.loc[:, 'snum'] = x[0]
+        res.loc[:, 'epoch'] = x[1]
+        res.loc[:, 'label'] = x[2]
+        res.loc[:, 'slice'] = j
+        results.append(res)
+    results = pd.concat(results)
+
+    results.to_hdf(x[4], 'decoding')
+    #results = results.groupby([u'predict_time', u'train_time', 'sensors', u'snum', u'epoch', 'label']).mean().reset_index()
+    return results
 
 
-for snum in range(1, 16):
-    for epoch in ['stimulus', 'response', 'feedback']:
-        for label in ['side', 'correct', 'noise_sigma', 'conf_r1', 'conf_rm1']:
-            for sensors in decoding.sensors.keys():
-                q.append((snum, epoch, label, sensors))
-
-results = executor.map(expand, q)
-diagnostics.progress(results)
-results = pd.concat(executor.gather(results))
-results = results.groupby([u'predict_time', u'train_time', u'snum', u'epoch', 'label']).mean().reset_index()
-results.to_hdf('generalization_across_time_additional.hdf', 'gat')
+def list_tasks(older_than='now'):
+    for snum in range(1, 16):
+        for epoch in ['stimulus']:
+            for label in ['contrast_probe']:
+                for sensors in decoding.sensors.keys():
+                    filename = 'results/decoding' + str(snum) + epoch + label + sensors + '.hdf'
+                    yield (snum, epoch, label, sensors, filename)
--- meg/decoding.py
+++ meg/decoding.py
@@ -1,7 +1,7 @@
 '''
 Do some decoding stuff.
 '''
-from conf_analysis.meg import preprocessing
+from conf_analysis.meg import preprocessing, tfr_analysis
 from conf_analysis.behavior import metadata
 from sklearn import cross_validation, svm, pipeline, preprocessing as skpre
 from sklearn import decomposition
@@ -11,7 +11,7 @@ from joblib import Memory
 
 
 sensors = dict(
-        all= lambda x:x,
+        all= lambda x: [ch for ch in x if ch.startswith('M')],
         occipital= lambda x:[ch for ch in x if ch.startswith('MLO') or ch.startswith('MRO')],
         posterior= lambda x:[ch for ch in x if ch.startswith('MLP') or ch.startswith('MRP')],
         central= lambda x:[ch for ch in x if ch.startswith('MLC')
@@ -33,9 +33,10 @@ def cv(x):
 
 
 def decode(classifier, data, labels, train_time, predict_times,
-        cv=cross_validation.StratifiedKFold, collapse=np.mean):
+        cv=cross_validation.StratifiedKFold, collapse=np.mean,
+        relabel_times=False):
     '''
-    Apply a classifier to data and predict labels with cross validation.
+    Apply a classifier to data [epochs x channels xtime] and predict labels with cross validation.
     Train classifier from data at data[:, :, train_time] and apply to all
     indices in predict_times. Indices are interpreted as an index into
     the data matrix.
@@ -99,18 +100,20 @@ def decode(classifier, data, labels, train_time, predict_times,
                 l2 = l2[:len(l1)]
             assert not any([k in l2 for k in l1])
             train_indices = np.concatenate([l1, l2])
-
         train = data[train_indices, :, train_time]
+
+        train_time_marker = train_time
         if len(train.shape) == 3:
             if collapse == 'reshape':
-                train = train.reshape((train.shape[0], np.prod(train.shape[1:])))
+                train = train.copy().reshape((train.shape[0], np.prod(train.shape[1:])))
             else:
                 train = collapse(train, axis=2)
-            train_time = train_time.stop
+            train_time_marker = train_time.stop-1
 
         clf=clf.fit(train, labels[train_indices])
 
         for pt in predict_times:
+            #print pt
             fold_result = {}
             test = data[test_indices, :, pt]
             if len(test.shape) == 3:
@@ -118,10 +121,15 @@ def decode(classifier, data, labels, train_time, predict_times,
                     test = test.reshape((test.shape[0], np.prod(test.shape[1:])))
                 else:
                     test = collapse(test, axis=2)
-                fold_result['predict_time'] = pt.stop
+                fold_result['predict_time'] = pt.stop-1
             else:
                 fold_result['predict_time'] = pt
-            fold_result['train_time'] = train_time
+            fold_result['train_time'] = train_time_marker
+
+            if relabel_times:
+                fold_result['train_time'] = relabel_times[fold_result['train_time']]
+                fold_result['predict_time'] = relabel_times[fold_result['predict_time']]
+            #print 'Test:', test.shape
             fold_result.update({
                 'fold':i,
                 'accuracy': clf.score(test, labels[test_indices])})
@@ -129,7 +137,8 @@ def decode(classifier, data, labels, train_time, predict_times,
     return pd.DataFrame(results)
 
 
-def generalization_matrix(epochs, labels, dt, classifier=clf, cv=cv, slices=False):
+def generalization_matrix(epochs, labels, dt,
+    classifier=clf, cv=cv, slices=False, baseline=None):
     '''
     Get data for a generalization across time matrix.
 
@@ -147,22 +156,28 @@ def generalization_matrix(epochs, labels, dt, classifier=clf, cv=cv, slices=Fals
     using time sequence data for decoding and function can be used
     to reduce time series data to single point.
     '''
+
     data = epochs._data
+
     sfreq = epochs.info['sfreq']
 
     tlen = data.shape[-1]/(float(sfreq)/1000.)
     nsteps = np.around(float(tlen)/dt)
     #steps = np.linspace(0, data.shape[-1]-1, nsteps).astype(int)
     steps = np.arange(0, data.shape[-1], int(data.shape[-1]/nsteps))
+    relabel_times = dict((k, v) for k, v in enumerate(epochs.time))
     if slices:
         steps = [slice(s, e) for s, e in zip(steps[0:-1], steps[1:])]
-        decoder = lambda x: decode(clf, data, labels, x, steps, cv=cv, collapse=slices)
+        decoder = lambda x: decode(clf, data, labels, x, steps, cv=cv,
+                collapse=slices, relabel_times=relabel_times)
     else:
-        decoder = lambda x: decode(clf, data, labels, x, steps, cv=cv)
+        decoder = lambda x: decode(clf, data, labels, x, steps, cv=cv,
+                relabel_times=relabel_times)
     return pd.concat([decoder(tt) for tt in steps])
 
 
-def apply_decoder(func, snum, epoch, label, channels=sensors['all']):
+def apply_decoder(func, snum, epoch, label, channels=sensors['all'],
+    label_func=None):
     '''
     Apply a decoder function to epochs from a subject and decode 'label'.
 
@@ -184,7 +199,7 @@ def apply_decoder(func, snum, epoch, label, channels=sensors['all']):
     '''
     s, m = preprocessing.get_epochs_for_subject(snum, epoch) #This will cache.
     s = s.pick_channels(channels(s.ch_names))
-    
+    times = s.times
     # Add confidence labels
     idx = m.response==1
     r1c = m.confidence.copy()
@@ -195,8 +210,15 @@ def apply_decoder(func, snum, epoch, label, channels=sensors['all']):
     m.loc[:, 'conf_r1'] = r1c
 
     # Drop nan labels
-    nan_loc = m.index[np.isnan(m.loc[:, label])]
-    use_loc = m.index[~np.isnan(m.loc[:, label])]
+    labels = m.loc[:, label]
+    if label_func is not None:
+
+        labels = label_func(labels)
+        nan_loc = m.index[np.isnan(labels)]
+        use_loc = m.index[~np.isnan(labels)]
+    else:
+        nan_loc = m.index[np.isnan(m.loc[:, label])]
+        use_loc = m.index[~np.isnan(m.loc[:, label])]
 
     m = m.drop(nan_loc)
     s = s[list(use_loc.astype(str))]
@@ -206,6 +228,94 @@ def apply_decoder(func, snum, epoch, label, channels=sensors['all']):
     if not all(s.events[:, 2] == m.index.values):
         raise RuntimeError('Indices of epochs and meta do not match! Task: ' + str(snum) + ' ' + epoch + ' ' + label)
     # Recode labels to 0-(n-1)
+
     labels = m.loc[:, label]
+    if label_func is not None:
+        labels = label_func(labels)
     labels = skpre.LabelEncoder().fit(labels).transform(labels)
     return func(s, labels)
+
+
+
+def to4d(a):
+    trial = toindex(np.unique(a.index.get_level_values('trial')))
+    channel = toindex(np.unique(a.index.get_level_values('channel')))
+    freqs = toindex(np.unique(a.index.get_level_values('freq')))
+    times = toindex(np.unique(a.columns.values))
+    out = np.empty((len(trial), len(channel), len(freqs), a.shape[1]))
+    for row in a.itertuples():
+        t, c, f, s = row[0]
+        out[trial[t], channel[c], freqs[f], :] = row[1:]
+    return out, (trial, channel, freqs, times)
+
+
+def toindex(x):
+    return dict((v, k) for k, v in enumerate(x))
+
+
+def tfr_apply_decoder(func, snum, epoch, label,
+    channels=sensors['all'], freq=(0, 150),
+    label_func=lambda x:x, time=(-0.5, 1.25), baseline=(-0.5, 0)):
+    '''
+    Apply a decoder function to TFR from a subject and decode 'label'.
+
+    Parameters
+    ----------
+        See apply_decoder
+    '''
+    s, m = tfr_analysis.get_subject(snum, freq, channels, time[0],  time[1],
+                    epoch=epoch)
+    s = s.groupby(level='channel').apply(lambda x: tfr_analysis.avg_baseline(x, baseline=baseline))
+
+
+    m = m.loc[~np.isnan(label_func(m[label])), :].sort_index()
+    label_index = list(np.sort(m.index.values))
+    m = m.loc[label_index, :]
+
+    s = s.sort_index()
+    s = s.loc[(label_index,), :]
+    #s = s.sort_index()
+
+    s, indices = to4d(s)
+    hashloc = toindex(m.index.values)
+    lval = m[label].values
+
+    labels = [lval[indices[0][i]] for i in m.index.values]
+    # Assert correct labelling
+    rs = dict((v, k) for k, v in indices[0].iteritems())
+
+    assert(
+        all(
+            [label_func(labels[i]) == label_func(m.loc[rs[i], label])
+                for i in np.arange(len(labels))]))
+    if label_func is not None:
+        labels = label_func(labels)
+
+    labels = skpre.LabelEncoder().fit(labels).transform(labels)
+    #s[labels==1, :, :, :] = np.random.randn(*s[labels==1, :, :, :].shape) + 1
+    #s[labels==0, :, :, :] = np.random.randn(*s[labels==0, :, :, :].shape) - 1
+    #return s, indices, labels
+    return func((s, indices), labels)
+
+
+def tfr_generalization_matrix(epochs, labels,
+    classifier=clf, cv=cv, dt = 1, slices='reshape', baseline=None, relabel_times=False):
+    '''
+    Get data for a generalization across time matrix.
+
+    epochs is a tuple: 4D array (trial, channel, freq, time), indexers
+        indexers are dicts that map keys to index.
+    '''
+
+    indexers = epochs[1]
+    data = epochs[0]
+    h, c, f, t = data.shape
+
+    data = data.reshape((h, c*f, t))
+    steps = [slice(t, t+dt) for t in range(data.shape[-1]-dt)]
+    print data.shape
+    print steps
+    relabel_times = dict((v, k) for k,v in indexers[-1].iteritems())
+    decoder = lambda x: decode(clf, data, labels, x, [x], cv=cv,
+        relabel_times=relabel_times)
+    return pd.concat([decoder(tt) for tt in steps])
