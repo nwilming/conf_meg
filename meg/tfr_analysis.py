f82620a0f062c0af8597f748b8dc0ef82c7a3d03
Niklas Wilming <niklaswilming@fastmail.fm>
2017-02-01 16:03:53 +0100

    Handy new functions to deal with tfr data

--- meg/tfr_analysis.py
+++ meg/tfr_analysis.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('/home/nwilming/')
 import glob
+from conf_analysis.behavior import metadata
 import mne, locale
 import pandas as pd
 import numpy as np
@@ -10,37 +11,54 @@ import pylab as plt
 import seaborn as sns
 from pymeg import tfr
 sns.set_style('ticks')
+from joblib import Memory
+memory = Memory(cachedir=metadata.cachedir)
 
-
-def get_df(files):
+def plot_contrast(df):
     '''
-    Load saved TFRs and prepare a data frame.
+    Make a plot that shows the dependence of gamma and contrast for a specific contrast
+    sample over time. In detail: plot contrast vs gamma.
     '''
     pass
 
+
+def contrast_dependence(df, contrast, edges=np.linspace(0.1, 0.9, 15)):
+    '''
+    Compute contrast vs. power in a freq band.
+
+    df is a data frame with time in columns and indexed by trial, channel and freq.
+    Data in df is assumed to be baseline corrected.
+    '''
+    # Bring data frame into trial x time form.
+    df = df.groupby(level='trial').mean()
+    #df = df.join(contrast)
+    centers = [low+(high-low)/2. for low, high in zip(edges[:-1], edges[1:])]
+    #print df.columns
+    return df.groupby(pd.cut(contrast, bins=edges, labels=centers)).mean()
 
 def get_all_tfr():
     pass
 
-import time
 
-def get_subs(freq, channel, tmin, tmax, epoch='stimulus', subs=range(1, 16)):
-    avgs = []
-    metas = []
-    for snum in subs:
-        start = time.time()
-        filenames = glob.glob('/home/nwilming/conf_meg/S%i/*%stfr.hdf5'%(snum, epoch))
-        metafiles = glob.glob('/home/nwilming/conf_meg/S%i/*%s.meta'%(snum, epoch))
-        df = tfr.get_tfrs(filenames, freq=freq, channel=channel, tmin=tmin, tmax=tmax)
-        print 'S%i loading %2.1fs'%(snum, time.time()-start)
-        avg = df.groupby(level=['freq', 'trial']).mean()
-        avg.loc[:, 'snum'] = snum
-        avg.set_index('snum', append=True, inplace=True)
-        print 'S%i grouping and setting index %2.1fs'%(snum, time.time()-start)
-        avgs.append(avg)
-        metas.append(pd.concat([pd.read_hdf(f) for f in metafiles]))
-        print 'S%i took %2.1fs'%(snum, time.time()-start)
-    return avgs, metas
+def get_subject(snum, freq, channel, tmin, tmax,
+                epoch='stimulus'):
+    filenames = glob.glob('/home/nwilming/conf_meg/S%i/*%stfr.hdf5'%(snum, epoch))
+    metafiles = glob.glob('/home/nwilming/conf_meg/S%i/*%s.meta'%(snum, epoch))
+    avg = tfr.get_tfrs(filenames, freq=freq, channel=channel, tmin=tmin, tmax=tmax)
+    avg.loc[:, 'snum'] = snum
+    avg.set_index('snum', append=True, inplace=True)
+    meta = pd.concat([pd.read_hdf(f) for f in metafiles])
+    return avg, meta
+
+
+def baseline(avg, id_base):
+    '''
+    Baseline correction by dividing by average baseline
+    '''
+    base = avg.loc[:, id_base].values.ravel().mean()
+    avg = np.log10(avg / base)
+    return avg
+
 
 def avg_baseline(avg, baseline):
     '''
@@ -48,10 +66,14 @@ def avg_baseline(avg, baseline):
     '''
     time =  avg.columns.get_level_values('time').values.astype(float)
     id_base = (baseline[0]<time) & (time<baseline[1])
+    #print id_base
     crs = []
+    avg = avg.apply(np.log10)
     # Baseline correction with average baseline across all trials. (base is freq specific)
     base = avg.loc[:, id_base].groupby(level='freq').mean().mean(1) # This should be len(#Freq)
-    div = lambda x: np.log10((x / base.loc[x.index.get_level_values('freq').values[0]]))
+    def div(x):
+        bval = base.loc[x.index.get_level_values('freq').values[0]]
+        return x - bval
     return avg.groupby(level='freq').apply(div)
 
 
@@ -59,13 +81,16 @@ def trial_baseline(avg, baseline):
     '''
     Baseline correction by dividing by per-trial baseline
     '''
+    index = avg.index
     time =  avg.columns.get_level_values('time').values.astype(float)
     id_base = (baseline[0]<time) & (time<baseline[1])
-    crs = []
-    # Baseline correction with average baseline across all trials. (base is freq specific)
     avg = np.log10(avg)
     base = avg.loc[:, id_base].groupby(level=['freq', 'trial']).mean().mean(1)
-    return avg.sub(base, axis=0)
+    levels = list(set(avg.index.names) - set(baseline.index.names))
+    avg.index = avg.index.droplevel(levels)
+    avg.div(base, axis=0, inplace=True)
+    avg.index = index
+    return avg
 
 
 def avg_contrast_resp(avg, meta, edges=np.linspace(0.2, 0.8, 7),
