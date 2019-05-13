3a5b1dadc792d85523737ca1a142131646aa3fb7
Niklas Wilming <niklaswilming@fastmail.fm>
2019-05-13 16:44:18 +0200

    Sync kernel analysis

--- conf_analysis/meg/cort_kernel.py
+++ conf_analysis/meg/cort_kernel.py
@@ -11,11 +11,18 @@ from conf_analysis.behavior import metadata
 from conf_analysis.behavior import empirical, kernels
 from conf_analysis.meg import regress
 from conf_analysis.meg import decoding_plots as dp, decoding_analysis as da
+from scipy.stats import linregress
+
+try:
+    import pylab as plt
+except:
+    plt = None
+
 
 from joblib import Memory
 
 if "TMPDIR" in os.environ.keys():
-    memory = Memory(cachedir=os.environ["PYMEG_CACHE_DIR"])
+    memory = Memory(cachedir=os.environ["PYMEG_CACHE_DIR"], verbose=0)
     inpath = "/nfs/nwilming/MEG/sr_labeled/aggs"
     outpath = "/nfs/nwilming/MEG/sr_decoding/"
 elif "RRZ_LOCAL_TMPDIR" in os.environ.keys():
@@ -27,6 +34,56 @@ else:
     inpath = "/home/nwilming/conf_meg/sr_labeled/aggs"
     outpath = "/home/nwilming/conf_meg/sr_decoding"
     memory = Memory(cachedir=metadata.cachedir)
+
+
+def prepare_and_save_data(freq_band=[45, 65], n_jobs=4, 
+    outpath='/home/nwilming/conf_analysis/results/cort_kernel.results.pickle'):
+    from joblib import Parallel, delayed
+    from conf_analysis.meg import srtfr
+    peaks = get_decoding_peaks()
+    ogl_areas = list(srtfr.get_ogl_clusters().keys())
+    ccs = Parallel(n_jobs=4)(
+        delayed(correlate)(
+            subject,
+            "Averaged",
+            cluster,
+            0.18,
+            peakslope=peaks,
+            freq_band=freq_band,
+            ogl=True,
+        )
+        for subject in range(1, 16)
+        for cluster in ogl_areas
+    )
+    ccs = pd.DataFrame(ccs)
+
+    rems = []
+    alls = []
+    nosplit_rems = []
+    nosplit_alls = []
+    from conf_analysis.meg import srtfr
+    clusters = [r for r in srtfr.get_clusters().keys() if not r.startswith("NSW")]
+    from itertools import product
+    tasks = []
+    for cluster, subject in product(clusters, np.arange(1, 16)):
+        tasks.append(delayed(d_gck)(
+            subject, "Averaged", cluster, 0.18, freq_band=freq_band, ogl=False,
+            remove_contrast_induced_flucts=False
+            )
+        )
+        tasks.append(delayed(d_gck)(
+            subject, "Averaged", cluster, 0.18, freq_band=freq_band, ogl=False, 
+            remove_contrast_induced_flucts=True
+            )
+        )
+    results = Parallel(n_jobs=4)(tasks)
+    
+   
+    K, C = get_kernels()
+    import pickle
+    kernels = pd.DataFrame(results)
+    pickle.dump({'ccs':ccs, 'kernels':kernels,  'K':K, 'v1decoding':peaks}, open(outpath, 'wb'))
+    return ccs, alls, rems, peaks, K
 
 
 def to_fluct(Xs, side):
@@ -45,6 +102,7 @@ def get_kernel(subject):
 
 @memory.cache()
 def get_kernels():
+    print(".")
     d = empirical.load_data()
     K = (
         d.groupby(["snum", "side"])
@@ -66,6 +124,32 @@ def _par_get_cortical_kernel(*args, **kwargs):
     return get_cortical_kernel(*args, **kwargs)
 
 
+def d_gck(subject,
+    hemi,
+    cluster,
+    latency,
+    remove_contrast_induced_flucts=False,
+    use_only_contrast_induced_flucts=False,
+    freq_band=[45, 65],
+    ogl=True,
+    split_by_mc=True):
+    
+    kernel = get_cortical_kernel(subject, hemi, cluster, latency,
+        remove_contrast_induced_flucts=remove_contrast_induced_flucts,
+        use_only_contrast_induced_flucts=use_only_contrast_induced_flucts,
+        freq_band=freq_band,
+        ogl=ogl,
+        split_by_mc=split_by_mc)
+
+    return {
+        'cluster': cluster,
+        'subject': subject,
+        'kernel': kernel,
+        'freq': freq_band,
+        'rmcif': remove_contrast_induced_flucts
+    }
+
+
 @memory.cache()
 def get_cortical_kernel(
     subject,
@@ -73,23 +157,37 @@ def get_cortical_kernel(
     cluster,
     latency,
     remove_contrast_induced_flucts=False,
+    use_only_contrast_induced_flucts=False,
     freq_band=[45, 65],
     ogl=True,
+    split_by_mc=True,
 ):
-
+    """
+    Computes how well fluctuations can separate responses for
+    each contrast conditions. A contrast condition is either
+    mean contrast > 0.5 or mean contrast < 0.5. The AUC
+    values for each contrast condition are averaged. 
+    """
     X, tpc, freq, meta = build_design_matrix(
         subject, cluster, latency, hemi, freq_bands=freq_band, ogl=ogl
     )
+
     if remove_contrast_induced_flucts:
         contrast = np.stack(meta.contrast_probe)
-        X = remove_contrast_induced_fluctuations(data, contrast)
-    idside = meta.loc[:, "side"] == 1
-    return (kernels.kernel(X[idside], meta.loc[idside, "response"])[0] - 0.5) - (
-        kernels.kernel(X[~idside], meta.loc[~idside, "response"])[0] - 0.5
-    )
+        X = X - get_contrast_induced_fluctuations(X, contrast)
+    if use_only_contrast_induced_flucts:
+        contrast = np.stack(meta.contrast_probe)
+        X = get_power_contrast_prediction(X, contrast)
+    if split_by_mc:
+        idside = meta.loc[:, "side"] == 1
+        a = kernels.kernel(X[idside], meta.loc[idside, "response"])[0] - 0.5
+        b = kernels.kernel(X[~idside], meta.loc[~idside, "response"])[0] - 0.5
+        return (a + b) / 2
+    else:
+        return kernels.kernel(X[:], meta.loc[:, "response"])[0] - 0.5
 
 
-def remove_contrast_induced_fluctuations(data, contrast):
+def get_contrast_induced_fluctuations(data, contrast):
     """
     Predict contrast induced fluctuations by means of linear regression.
 
@@ -100,8 +198,29 @@ def remove_contrast_induced_fluctuations(data, contrast):
     data = data.copy()
     for i in range(10):
         slope, intercept, _, _, _ = linregress(contrast[:, i], data[:, i])
-        data[:, i] -= slope * data[:, i] + intercept
+        data[:, i] = slope * contrast[:, i] + intercept
     return data
+
+
+def get_power_contrast_prediction(data, contrast):
+    """
+    Predict contrast induced fluctuations by means of linear regression.
+
+    Data is n_trials x 10 matrix of power values
+    contrast is n_trials x 10 matrix of contrast values
+
+    """
+    data = data.copy()
+    for i in range(10):
+        slope, intercept, _, _, _ = linregress(data[:, i], contrast[:, i])
+        data[:, i] = slope * data[:, i] + intercept
+    return data
+
+
+@memory.cache
+def get_decoding_peaks():
+    ssd = dp.get_ssd_data(ogl=True, restrict=False)
+    return dp.extract_peak_slope(ssd).test_slope.Pair
 
 
 def correlate(
@@ -130,7 +249,7 @@ def correlate(
             latency,
             freq_band=freq_band,
             ogl=ogl,
-            remove_contrast_induced_fluctuations=True,
+            remove_contrast_induced_flucts=True,
         )
         ck_all = get_cortical_kernel(
             subject,
@@ -139,10 +258,10 @@ def correlate(
             latency,
             freq_band=freq_band,
             ogl=ogl,
-            remove_contrast_induced_fluctuations=False,
+            remove_contrast_induced_flucts=False,
         )
-        res["choice_corr_cif_removed"] = np.corrcoef(ck_int.ravel(), k.ravel())[0, 1]
-        res["choice_corr_with_cif"] = np.corrcoef(ck_all.ravel(), k.ravel())[0, 1]
+        res["AF-CIFcorr"] = np.corrcoef(ck_int.ravel(), k.ravel())[0, 1]
+        res["AFcorr"] = np.corrcoef(ck_all.ravel(), k.ravel())[0, 1]
 
     except RuntimeError:
         # Area missing
@@ -150,11 +269,14 @@ def correlate(
 
     if peakslope is not None:
         # vfcpeak = dp.extract_peak_slope(ssd)
-        pvt = peak.loc[:, cluster]
-        assert pvt.shape == (10, 15)
-        res["choice_corr_contrast_induced"] = np.corrcoef(
-            K.loc[sub, :].values, pvt.loc[:, sub].values
-        )[0, 1]
+        if cluster in peakslope:
+            pvt = peakslope.loc[:, cluster]
+            assert pvt.shape == (10, 15)
+            res["DCDcorr"] = np.corrcoef(k.ravel(), pvt.loc[:, subject].values)[0, 1]
+            res["DCD-AFcorr"] = np.corrcoef(ck_all.ravel(), pvt.loc[:, subject].values)[
+                0, 1
+            ]
+
     return res
 
 
@@ -186,10 +308,12 @@ def build_design_matrix(
         data.index = index
         data.head()
 
-    trial_index = data.index.get_level_values("trial")
+    trial_index = data.index.get_level_values("trial").unique()
+
     X, time_per_col = regress.prep_low_level_data(
         cluster, data, 0, latency, trial_index, freq_bands=freq_bands
     )
+    print(X.shape)
     cols = X.columns.values
     index = X.index.values
     X = X.values
@@ -200,3 +324,22 @@ def build_design_matrix(
         X = (X - X.mean(0)) / X.std(0)
 
     return X, time_per_col, cols, meta
+
+def plotwerr(pivottable, *args, ax=None, label=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    N = pivottable.shape[0]
+    x = pivottable.columns.values
+    mean = pivottable.mean(0).values
+    std = pivottable.std(0).values
+    sem = std/(N**.5)
+    ax.plot(x, mean, *args, label=label, **kwargs)
+    if 'alpha' in kwargs:
+        del kwargs['alpha']
+    if 'color' in kwargs:
+        color = kwargs['color']
+        del kwargs['color']
+        ax.fill_between(x, mean+sem, mean-sem, facecolor=color, edgecolor='none', alpha=0.5, **kwargs)    
+    else:
+        ax.fill_between(x, mean+sem, mean-sem, edgecolor='none', alpha=0.5, **kwargs) 
+
